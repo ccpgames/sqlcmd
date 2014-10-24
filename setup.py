e3067b55d18ed831525e85e8855e6df2ce7b86bd
ccpmatti <matti@ccpgames.com>
2014-10-24 15:01:02 +0000

    Updating license and making 'con' the default input.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='sqlcmd',
-      version='0.1',
+      version='0.0.2',
       description="Pure Python alternative to SQLCMD.EXE using PyTDS",
       long_description=open("README.md").read() + "\n"
       + open("AUTHORS.md").read() + "\n" + open("CHANGES.md").read(),
@@ -11,5 +11,5 @@ setup(name='sqlcmd',
       py_modules=['sqlcmd'],
       entry_points={"console_scripts": ["sqlcmd = sqlcmd:main"]},
       install_requires=['python-tds>=1.7.2'],
-      license='GNU GPLv3'
+      license='MIT'
       )
--- sqlcmd.py
+++ sqlcmd.py
@@ -1,16 +1,3 @@
-# sqlcmd 0.1
-#
-# Copyright (C) 2014 CCP Games.
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
 import argparse
 
 import pytds
@@ -42,7 +29,7 @@ def execute_sql_file(
     with open(input_file, "r") as sqlfile:
         sqltext = sqlfile.read()
 
-    blocks = sqltext.split("GO\n")
+    blocks = sqltext.split("\nGO\n")
 
     # Strip trailing GO statement
     if not blocks[-1]:
@@ -83,7 +70,7 @@ def main():
     )
 
     parser.add_argument(
-        '-i', '--input-file', action='store', nargs='?', default=None,
+        '-i', '--input-file', action='store', nargs='?', default="CON",
         help="Input file."
     )
 
